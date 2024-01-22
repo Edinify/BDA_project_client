2151705d993a4ce2050006d9039a3af319f15e84
Edinify <141638391+Edinify@users.noreply.github.com>
2024-01-23 01:58:47 +0400

    Update index.js

--- src/apiRoot/index.js
+++ src/apiRoot/index.js
@@ -1,3 +1,4 @@
-export const apiRoot = "http://api-server:4000/api"
+export const apiRoot = https://bdadb.azurewebsites.net/api
+//"http://api-server:4000/api"
 // "http://localhost:4000/api"; enteskcrmserver.azurewebsites.net
 // 
