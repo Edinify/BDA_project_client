099e68016463ae8f0c19bed06173527f0a43cdd6
SamedovResul <samedovrasul7@gmail.com>
2024-02-09 13:40:41 +0400

    .

--- src/apiRoot/index.js
+++ src/apiRoot/index.js
@@ -1,4 +1,1 @@
 export const apiRoot = "https://lemon-sea-047347403.4.azurestaticapps.net/api";
-//"http://api-server:4000/api"
-// "http://localhost:4000/api";
-// 'https://bdadb.azurewebsites.net/api'
