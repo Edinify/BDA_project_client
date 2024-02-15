9d8fa68e716ab4ec873758ab91e9f510e9fbe167
xeyyamomarov <xeyyamomarov2014@gmail.com>
2024-02-15 12:28:46 +0400

    logo was changed

--- src/Layout/Sidebar/components/SidebarHead/SidebarHead.jsx
+++ src/Layout/Sidebar/components/SidebarHead/SidebarHead.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import { useSelector } from "react-redux";
 // import { ReactComponent as LogoIcon } from "../../../../assets/icons/sidebar-head-logo.svg";
-import { ReactComponent as LogoIcon } from "../../../../assets/icons/logo/bdaLogoMid.svg";
+// import { ReactComponent as LogoIcon } from "../../../../assets/icons/logo/bdaLogoMid.svg";
+import { ReactComponent as LogoIcon } from "../../../../assets/icons/logo/whiteLogoMid.svg";
 import UserAvatar from "../../../../assets/images/user-avatar.jpg";
 
 const SidebarHead = ({ closeSidebar }) => {
