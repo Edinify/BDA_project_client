79e9dd59dd30ad3089612f6e2bfc9da346eddbf6
SamedovResul <samedovrasul7@gmail.com>
2024-02-27 23:48:08 +0400

    add practic lesson

--- src/apiRoot/index.js
+++ src/apiRoot/index.js
@@ -2,4 +2,9 @@ export const apiRoot = "https://bdadb.azurewebsites.net/api";
 //"http://api-server:4000/api"
 // "http://localhost:4000/api";
 // 'https://bdadb.azurewebsites.net/api'
+
+
 // https://bdaserverdev.azurewebsites.net/api
+
+
+
