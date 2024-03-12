e5727a856d1ec25376dcd9c71407a3c74018a69a
Edinify <141638391+Edinify@users.noreply.github.com>
2024-03-12 13:09:14 +0400

    Update index.js

--- src/apiRoot/index.js
+++ src/apiRoot/index.js
@@ -1,10 +1,5 @@
-export const apiRoot = "https://bdadb.azurewebsites.net/api";
+export const apiRoot = "http://192.168.1.2/api";
 //"http://api-server:4000/api"
 // "http://localhost:4000/api";
-// 'https://bdadb.azurewebsites.net/api'
-
-
-// https://bdaserverdev.azurewebsites.net/api
-
 
 
