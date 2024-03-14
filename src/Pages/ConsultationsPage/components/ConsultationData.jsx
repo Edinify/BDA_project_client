ea0e32c73b713cbfb3d5b38c2a3ec28effd5e08d
xeyyamomarov <xeyyamomarov2014@gmail.com>
2024-03-14 16:13:38 +0400

    /consultation page was changed

--- src/Pages/ConsultationsPage/components/ConsultationData.jsx
+++ src/Pages/ConsultationsPage/components/ConsultationData.jsx
@@ -5,6 +5,8 @@ import { Pagination } from "antd";
 import Loading from "../../../globalComponents/Loading/Loading";
 import MoreModal from "../../../globalComponents/MoreModal/MoreModal";
 import ConfirmModal from "../../../globalComponents/ConfirmModal/ConfirmModal";
+import InfiniteScroll from "react-infinite-scroll-component";
+import SmallLoading from "../../../globalComponents/Loading/components/SmallLoading/SmallLoading";
 
 const ConsultationData = ({ pageNum, getPageNumber, userData }) => {
   const { totalPages, loading, consultationData } = useSelector(
@@ -18,14 +20,19 @@ const ConsultationData = ({ pageNum, getPageNumber, userData }) => {
     "Təlimçi",
     "Mobil nömrə",
     "İxtisas",
+    "Persona",
+    "Sahə biliyi",
     "Əlaqə tarixi",
     "Konsultasiya tarixi",
     "Konsultasiya saatı",
+    "Ləğv səbəbi",
+    "Əlavə məlumat",
+    "Bizi haradan eşitdiniz",
     "Status",
     "",
   ];
 
-  console.log(consultationData,"consultation data")
+  console.log(consultationData, "consultation data");
 
   useEffect(() => {
     if (openMoreModal) {
@@ -50,8 +57,21 @@ const ConsultationData = ({ pageNum, getPageNumber, userData }) => {
           )}
 
           {openConfirmModal && <ConfirmModal type="consultation" />}
+          
 
           <div className="table-con">
+          <InfiniteScroll
+            style={{ overflowX: "none" }}
+            dataLength={consultationData.length}
+            // next={getNextStudents}
+            // hasMore={totalLength > consultationData.length || loading}
+            hasMore={consultationData.length || loading}
+            loader={<SmallLoading />}
+            endMessage={
+              <p style={{ textAlign: "center", fontSize: "20px" }}></p>
+            }
+            scrollThreshold={1}
+          >
             <table className="details-table consultation-page ">
               <thead>
                 <tr>
@@ -73,6 +93,7 @@ const ConsultationData = ({ pageNum, getPageNumber, userData }) => {
                 ))}
               </tbody>
             </table>
+            </InfiniteScroll>
           </div>
 
           <div className="details-list-tablet with-more">
