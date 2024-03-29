d5db7f2560c55bf5a848aaa4ca2694c231a6ac76
Ceyhun Resulov <ceyhunresulov23@gmail.com>
2024-03-29 16:35:15 +0400

    add export excel action

--- src/globalComponents/GlobalHead/StudentPageHead/StudentPageHead.jsx
+++ src/globalComponents/GlobalHead/StudentPageHead/StudentPageHead.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { CoursesDropdown } from "../CoursesDropdown/CoursesDropdown";
 import { GroupsDropdown } from "../GroupsDropdown/GroupsDropdown";
 import { ReactComponent as HalfCircleICon } from "../../../assets/icons/filter/half-circle-svgrepo-com.svg";
+import { downloadExcelAction } from "../../../redux/actions/studentsActions";
 
 const StudentPageHead = ({ filter, count }) => {
   return (
@@ -17,6 +18,13 @@ const StudentPageHead = ({ filter, count }) => {
         <p className="filter-count">{count || 0}</p>
         <HalfCircleICon />
       </div>
+
+      <button
+        onClick={() => downloadExcelAction()}
+        style={{ border: "1px solid orange", cursor: "pointer" }}
+      >
+        excel
+      </button>
     </div>
   );
 };
--- src/redux/actions/studentsActions.js
+++ src/redux/actions/studentsActions.js
@@ -590,3 +590,20 @@ export const downloadContractAction = async ({
     window.URL.revokeObjectURL(url);
   } catch (error) {}
 };
+
+export const downloadExcelAction = async () => {
+  try {
+    const response = await API.get(`/excel`, {
+      responseType: "blob",
+    });
+
+    const url = window.URL.createObjectURL(new Blob([response.data]));
+
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "students.xlsx");
+    document.body.appendChild(link);
+    link.click();
+    link.parentNode.removeChild(link);
+  } catch (error) {}
+};
